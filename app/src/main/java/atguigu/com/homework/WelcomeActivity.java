20b5b7b60f0842fe510eb602a34710c18b30e44c
zhaokaile123 <zhaokaile011206>
2017-05-30 10:37:29 +0800

    欢迎界面以及主页面的布局框架完成

--- app/src/main/java/atguigu/com/homework/WelcomeActivity.java
+++ app/src/main/java/atguigu/com/homework/WelcomeActivity.java
@@ -1,7 +1,11 @@
 package atguigu.com.homework;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        handler.sendEmptyMessageDelayed(0, 2000);
+
+    }
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+        }
+    };
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+
+        return super.onTouchEvent(event);
     }
 }
---
+++ app/src/main/java/atguigu/com/homework/view/TitleView.java
@@ -0,0 +1,22 @@
+package atguigu.com.homework.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by ASUS on 2017/5/30.
+ */
+
+public class TitleView extends LinearLayout {
+
+    public TitleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+    }
+
+}
